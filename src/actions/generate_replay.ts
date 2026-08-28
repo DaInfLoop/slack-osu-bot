e7180245c5a83c452feb1fa8447e525f597d0171
DaInfLoop <github@dainfloop.is-a.dev>
2026-08-28 18:55:53 +0100

    Make it so the bot can actually reply when it isn't in the channel

--- src/actions/generate_replay.ts
+++ src/actions/generate_replay.ts
@@ -7,16 +7,15 @@ import { queue, replayRenderTask } from "../../replay-handler";
 import type { OsuScore } from "../commands/osu-lastplayed";
 
 export default async function GenerateReplay(ctx: SlackActionMiddlewareArgs<BlockButtonAction> & AllMiddlewareArgs<StringIndexed>) {
-    await ctx.ack();
-
     const channelInfo = await ctx.client.conversations.info({
         channel: ctx.body.channel?.id!
     });
+    
+    await ctx.ack();
 
     if (!channelInfo.ok) {
-        return ctx.client.chat.postEphemeral({
-            channel: ctx.body.channel?.id!,
-            user: ctx.body.user.id,
+        return ctx.respond({
+            response_type: 'ephemeral',
             text: `:warning: I can't send messages in this channel. Please invite me to the channel and try again.`
         });
     }
@@ -31,17 +30,15 @@ export default async function GenerateReplay(ctx: SlackActionMiddlewareArgs<Bloc
     const _replay = await osr.read(replayBuffer);
 
     if (queue.some(item => item.md5 === _replay.replayMD5)) {
-        return ctx.client.chat.postEphemeral({
-            channel: ctx.body.channel?.id!,
-            user: ctx.body.user.id,
+        return ctx.respond({
+            response_type: 'ephemeral',
             text: `:warning: This replay has already been queued for rendering.`
         });
     }
 
     if (_replay.gameMode !== 0) {
-        return ctx.client.chat.postEphemeral({
-            channel: "C165V7XT9",
-            user: ctx.body.user.id,
+        return ctx.respond({
+            response_type: 'ephemeral',
             text: `:warning: *Hey <@${ctx.body.user.id}>!* Unfortunately, o!rdr doesn't support replays other than :osu-standard: osu!standard replays, so I can't render this replay. Sorry!`
         });     
     }
@@ -56,9 +53,8 @@ export default async function GenerateReplay(ctx: SlackActionMiddlewareArgs<Bloc
         if (err.code == 'ENOENT') {
             await fs.promises.mkdir('.replay')
         } else {
-            return ctx.client.chat.postEphemeral({
-                channel: "C165V7XT9",
-                user: ctx.body.user.id,
+            return ctx.respond({
+                response_type: 'ephemeral',
                 text: `:warning: *Hey <@${ctx.body.user.id}>!* An unexpected error occurred while trying to handle your replay. Contact the bot maintainer. (${err.code})`
             });
         }
@@ -80,9 +76,8 @@ export default async function GenerateReplay(ctx: SlackActionMiddlewareArgs<Bloc
             fromLastPlayed: true
         })
 
-        ctx.client.chat.postEphemeral({
-            channel: "C165V7XT9",
-            user: ctx.body.user.id,
+        await ctx.respond({
+            response_type: 'ephemeral',
             text: `:white_check_mark: This replay is now queued for rendering.`
         });
 
