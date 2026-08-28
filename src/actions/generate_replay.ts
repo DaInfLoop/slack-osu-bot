9c32cd537a5a2fa325b2ae12eb6ded6c1e26136e
DaInfLoop <github@dainfloop.is-a.dev>
2026-08-28 19:51:27 +0100

    Extra error handling when not in channel

--- src/actions/generate_replay.ts
+++ src/actions/generate_replay.ts
@@ -7,16 +7,36 @@ import { queue, replayRenderTask } from "../../replay-handler";
 import type { OsuScore } from "../commands/osu-lastplayed";
 
 export default async function GenerateReplay(ctx: SlackActionMiddlewareArgs<BlockButtonAction> & AllMiddlewareArgs<StringIndexed>) {
-    const channelInfo = await ctx.client.conversations.info({
-        channel: ctx.body.channel?.id!
-    });
-    
     await ctx.ack();
+    try {
+        const channelInfo = await ctx.client.conversations.info({
+            channel: ctx.body.channel?.id!
+        });
+        
+        if (!channelInfo.ok) {
+            return ctx.client.chat.postMessage({
+                channel: ctx.body.user.id,
+                text: `:warning: You tried to generate <https://${ctx.body.team?.domain}.slack.com/archives/${ctx.body.channel?.id}/p${ctx.body.message?.ts.replace('.', '')}|a replay>, but I can't send messages in <#${ctx.body.channel?.id}>. Please invite me to the channel and try again.`
+            });
+        }
+    } catch (e) {
+        const err = e as NodeJS.ErrnoException & { data?: {
+            ok: false,
+            error: string
+        } };
 
-    if (!channelInfo.ok) {
-        return ctx.respond({
-            response_type: 'ephemeral',
-            text: `:warning: I can't send messages in this channel. Please invite me to the channel and try again.`
+        if (err.code === 'slack_webapi_platform_error' && err.data?.error === 'channel_not_found')
+            return ctx.client.chat.postMessage({
+                channel: ctx.body.user.id,
+                unfurl_links: true,
+                text: `:warning: You tried to generate <https://${ctx.body.team?.domain}.slack.com/archives/${ctx.body.channel?.id}/p${ctx.body.message?.ts.replace('.', '')}|a replay>, but I can't send messages in <#${ctx.body.channel?.id}>. Please invite me to the channel and try again.`
+            });
+
+        console.error(err);
+        return ctx.client.chat.postMessage({
+            channel: ctx.body.user.id,
+            unfurl_links: true,
+            text: `:warning: An unexpected error occurred while trying to generate <https://${ctx.body.team?.domain}.slack.com/archives/${ctx.body.channel?.id}/p${ctx.body.message?.ts.replace('.', '')}|a replay>. Contact the bot maintainer.`
         });
     }
 
@@ -40,7 +60,7 @@ export default async function GenerateReplay(ctx: SlackActionMiddlewareArgs<Bloc
         return ctx.respond({
             response_type: 'ephemeral',
             text: `:warning: *Hey <@${ctx.body.user.id}>!* Unfortunately, o!rdr doesn't support replays other than :osu-standard: osu!standard replays, so I can't render this replay. Sorry!`
-        });     
+        });
     }
 
     // ensure .replays folder exists
